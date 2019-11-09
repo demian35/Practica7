20aef6f5aed35ea6f99eb233515a873b5f6674e2
demian35 <josedemian@ciencias.unam.mx>
2019-11-08 21:00:43 -0600

    Agregue metodo para crear una matriz

--- Matriz.py
+++ Matriz.py
@@ -1,9 +1,24 @@
 class Matriz(object):
 
-    def Matriz(self, altura, largo)
-        pass
+	def createList(self,n):
+		listamatriz=[]
+		for i in range(n):
+			listamatriz.append(random.randint(0,100))
+		return listamatriz	
 
-    def busca(self, coorAltura, coorLargo):
-        pass 
+	def Matriz(self, m,l ):
+		matriz= []
+		for i in range(m):
+			matriz.append(createList(l))
+		for i in range(m):
+			fila= ""
+			for j in range(l):
+				espacios= ' '*(5-len(str(matriz[i][j])))
+				fila= fila+espacios+str(matriz[i][j])
+			print(fila)
+		return matriz		
 
-    def __init__(self, arg):
+	def busca(self, coorAltura, coorLargo):
+		pass 
+
+    #def __init__(self, arg):
